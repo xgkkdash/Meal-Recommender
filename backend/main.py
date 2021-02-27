8ee351f02c5cb4e75fd87842d3194902c1260c71
xgkkdash <48404201+xgkkdash@users.noreply.github.com>
2021-02-28 01:02:03 +0800

    return a static html using fastapi

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,13 @@
+import codecs
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def read_root():
-    return {"Hello": "World"}
+    with codecs.open("hello_world.html", "r", "utf-8") as f:
+        html_content = f.read()
+        return HTMLResponse(content=html_content, status_code=200)
