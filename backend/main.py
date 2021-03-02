3d8c35c787310f25e78e4859c5cdf5403b3adab1
xgkkdash <48404201+xgkkdash@users.noreply.github.com>
2021-03-03 03:52:22 +0800

    add main method in backend

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,7 @@
 import codecs
+import os
 
+import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
@@ -11,3 +13,8 @@ def read_root():
     with codecs.open("hello_world.html", "r", "utf-8") as f:
         html_content = f.read()
         return HTMLResponse(content=html_content, status_code=200)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 8000))
+    uvicorn.run("main:app", host='0.0.0.0', port=port, reload=True)
