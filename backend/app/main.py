978c4355cbcf10c6588dc2b4146f7bc5d528fb5e
xgkkdash <48404201+xgkkdash@users.noreply.github.com>
2021-03-12 10:16:04 +0800

    fix startup command in main

--- backend/app/main.py
+++ backend/app/main.py
@@ -10,4 +10,4 @@ app.include_router(items.router)
 
 
 if __name__ == '__main__':
-    uvicorn.run("app:app", host='0.0.0.0', port=settings.PORT, reload=True)
+    uvicorn.run("main:app", host='0.0.0.0', port=settings.PORT, reload=True)
