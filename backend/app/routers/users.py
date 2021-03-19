79d6e73e64f8e66ec80e86ba0027d0a7760ee7ff
xgkkdash <48404201+xgkkdash@users.noreply.github.com>
2021-03-19 09:48:27 +0800

    implement user apis

--- backend/app/routers/users.py
+++ backend/app/routers/users.py
@@ -1,18 +1,35 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, Depends
+
+from app.database import get_database
+from app.database.users import find_all_users, find_user, delete_user
+from app.utils import get_user_by_token
 
 router = APIRouter(tags=["users"])
 
 
 @router.get("/users")
-async def get_all_users():
-    pass
+async def get_all_users(user=Depends(get_user_by_token), db=Depends(get_database)):
+    if user.role != "admin":
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    all_users = await find_all_users(db)
+    if not all_users:
+        raise HTTPException(status_code=400, detail="Not Found")
+    return all_users
 
 
 @router.get("/users/{account_id}")
-async def get_user():
-    pass
+async def get_user(account_id: str, user=Depends(get_user_by_token), db=Depends(get_database)):
+    if user.role != "admin":
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    target_user = await find_user(db, account_id)
+    if not target_user:
+        raise HTTPException(status_code=400, detail="Not Found")
+    return target_user
 
 
 @router.delete("/users/{account_id}")
-async def remove_user():
-    pass
+async def remove_user(account_id: str, user=Depends(get_user_by_token), db=Depends(get_database)):
+    if user.role != "admin":
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    await delete_user(db, account_id)
+    return {"success": True}
